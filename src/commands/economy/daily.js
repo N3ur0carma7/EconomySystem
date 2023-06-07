801b2dcba5bc23f001314a46b00fc39994813c52
N3ur0carma7 <matthieu.bgs.fr@gmail.com>
2023-06-07 21:27:17 +0200

    daily commands

---
+++ src/commands/economy/daily.js
@@ -0,0 +1,63 @@
+const { Client, Interaction } = require('discord.js');
+const User = require('../../models/User');
+
+const dailyAmount = 1000;
+
+module.exports = {
+  name: 'daily',
+  description: 'Collecte ta récompense quotidienne.',
+  devOnly: true,
+  /**
+   *
+   * @param {Client} client
+   * @param {Interaction} interaction
+   */
+  callback: async (client, interaction) => {
+    if (!interaction.inGuild()) {
+      interaction.reply({
+        content: 'You can only run this command inside a server.',
+        ephemeral: true,
+      });
+      return;
+    }
+
+    try {
+      await interaction.deferReply();
+
+      const query = {
+        userId: interaction.member.id,
+        guildId: interaction.guild.id,
+      };
+
+      let user = await User.findOne(query);
+
+      if (user) {
+        const lastDailyDate = user.lastDaily.toDateString();
+        const currentDate = new Date().toDateString();
+
+        if (lastDailyDate === currentDate) {
+          interaction.editReply(
+            'Tu as déjà récupéré ta récompense quotidienne. Reviens demain !'
+          );
+          return;
+        }
+        
+        user.lastDaily = new Date();
+      } else {
+        user = new User({
+          ...query,
+          lastDaily: new Date(),
+        });
+      }
+
+      user.balance += dailyAmount;
+      await user.save();
+
+      interaction.editReply(
+        `${dailyAmount} kastocoins ont été ajoutés à votre compte. Le nouveau montant de votre compte est de ${user.balance} kastocoins.`
+      );
+    } catch (error) {
+      console.log(`Error with /daily: ${error}`);
+    }
+  },
+};
