037072a93f987807214bede33020712cc2eb4834
N3ur0carma7 <matthieu.bgs.fr@gmail.com>
2023-06-07 19:21:14 +0200

    Edited Index.js + Added Handler

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 require('dotenv/config');
 const { Client, IntentsBitField } = require('discord.js');
+const mongoose = require('mongoose');
+const eventHandler = require('../handlers/eventHandler');
 
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildPresences,
+    ],
+});
 
-client.login(process.env.TOKEN); // Permet de mettre le bot en ligne avec son token caché dans un fichier ".env" (Raisons de sécurité).
+(async () =>{
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(process.env.MONGODB_URI, { keepAlive: true });
+        console.log("Connected to DB");
+
+        eventHandler(client);
+
+        client.login(process.env.TOKEN); // Permet de mettre le bot en ligne avec son token caché dans un fichier ".env" (Raisons de sécurité).
+    } catch (error) {
+        console.log(`Error: ${error}`);
+    }
+})
+
